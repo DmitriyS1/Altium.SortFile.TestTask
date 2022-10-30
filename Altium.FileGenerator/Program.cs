665c08e8e998f23a2ab33940d4e37a6d62980d31
Dmitrii Semenov <dmitriy.a.semenov@outlook.com>
2022-10-30 13:46:05 +0200

    Working File generator

--- Altium.FileGenerator/Program.cs
+++ Altium.FileGenerator/Program.cs
@@ -1,46 +1,9 @@
-﻿using Altium.FileGenerator.Models;
+﻿using Altium.FileGenerator.Services;
 
 Console.WriteLine("File generator");
 Console.WriteLine("Created file will have next format: \"Number. String\" for each line, i.e. 134. Apple");
 
-var isCorrectInput = false;
-while (!isCorrectInput)
-{
-    Console.WriteLine("Enter size of the file in the format 100M - for 100 MB file, 10G - for 10 GB file");
+var size = MenuService.GetFileSize();
 
-    var sizeString = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(sizeString))
-    {
-        ErrorMessages.PrintFormatError();
-        continue;
-    }
-
-    var lastChar = sizeString[sizeString.Length - 1];
-    if (lastChar != 'M' || lastChar != 'G')
-    {
-        ErrorMessages.PrintFormatError();
-        continue;
-    }
-
-    var stringNumber = sizeString.Remove(sizeString.Length - 1, 1);
-    var success = int.TryParse(stringNumber, out var number);
-    if (!success)
-    {
-        ErrorMessages.PrintFormatError();
-        continue;
-    }
-
-    var count = 0L;
-    switch (lastChar)
-    {
-        case 'M':
-            count = 512 * number;
-            break;
-        default:
-            count = 512 * 1024 * number;
-            break;
-    }
-
-
-}
-
+var fileGenerator = new FileGenerator();
+fileGenerator.Run(size);
---
+++ Altium.FileGenerator/Services/MenuService.cs
@@ -0,0 +1,54 @@
+﻿using Altium.FileGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altium.FileGenerator.Services
+{
+    public static class MenuService
+    {
+        public static long GetFileSize()
+        {
+            var isCorrectInput = false;
+            var count = 0L;
+            while (!isCorrectInput)
+            {
+                Console.WriteLine("Enter size of the file in the format 100M - for 100 MB file, 10G - for 10 GB file");
+
+                var sizeString = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(sizeString))
+                {
+                    ErrorMessages.PrintFormatError();
+                    continue;
+                }
+
+                var lastChar = sizeString[sizeString.Length - 1];
+                if (lastChar != 'M' && lastChar != 'G')
+                {
+                    ErrorMessages.PrintFormatError();
+                    continue;
+                }
+
+                var stringNumber = sizeString.Remove(sizeString.Length - 1, 1);
+                var success = int.TryParse(stringNumber, out var number);
+                if (!success)
+                {
+                    ErrorMessages.PrintFormatError();
+                    continue;
+                }
+
+                count = lastChar switch
+                {
+                    'M' => 1024L * 1024L * number,
+                    _ => 1024L * 1024L * 1024L * number,
+                };
+
+                isCorrectInput = true;
+            }
+
+            return count;
+        }
+    }
+}
