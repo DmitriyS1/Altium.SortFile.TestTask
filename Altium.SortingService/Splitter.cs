cb02115de8fcfec7aaf2cf2d4bd553e496db8039
Dmitrii Semenov <dmitriy.a.semenov@outlook.com>
2022-11-06 10:22:56 +0200

    Added function to find last created file

---
+++ Altium.SortingService/Splitter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altium.SortingService
+{
+    public class Splitter
+    {
+        public string WorkingFileName()
+        {
+            return GetFileName();
+        }
+
+        private string GetFileName()
+        {
+            var directory = new DirectoryInfo("../../../../Altium.FileGenerator/bin/Debug/net6.0");
+            var myFile = directory.GetFiles()
+                .OrderByDescending(f => f.LastWriteTime)
+                .FirstOrDefault(x => x.Name.Contains("file-"));
+
+            return myFile.Name;
+        }
+    }
+}
